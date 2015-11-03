545a941764f9a826e4359c4b24c70c48d0fe1c40
Nishant Shah <shah.nishant92@gmail.com>
2015-11-03 00:12:50 -0500

    Some more code

--- radio.py
+++ radio.py
@@ -4,12 +4,29 @@ f = open("adjacent-states","r")
 states = dict()
 for line in f:
 	states[line.split()[0]] = tuple(line.split()[1:])
+
+'''
+for key in states.keys():
+	print key,len(states[key])
+print  max(states, key = lambda k: len(states[k]))
+'''
 #domain value in a CSP	
 domain = ('A','B','C','D')
 #states_freq stores a state as a key and possible assignment of frequencies to that particular state
 states_freq = dict()
 for key in states.keys():
 	states_freq[key] = list(domain)
-max_state = max(states, key = lambda k: len(states[k]))	
-print states[max_state]
+#Return the state having maximum no of neighbourhood states and unassigned frequency  
+def selUnassignedVar(states,states_freq):
+	#print max(states, key = lambda k: len(states[k]))
+	return max(states, key = lambda k: len(states[k]) + len(states_freq[k]))
+
+assignment = dict()
+def arc_consistency(states,states_freq,assignment):
+
+#Solves CSP
+def csp(states,domain):
+	pass
+
+
 
