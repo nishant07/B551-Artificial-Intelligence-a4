a54ee4682fa68aefad22ff882a0f79c3a5cffd13
Nishant Shah <shah.nishant92@gmail.com>
2015-11-03 13:51:37 -0500

    Full logic with full bugs

--- radio.py
+++ radio.py
@@ -12,8 +12,9 @@ states_domain = dict()
 for key in states.keys():
 	states_domain[key] = list(domain)
 assignment = dict()
-def assign(state,frequency,assignment):
+def assign(state,frequency,assignment):	
 	assignment[state] = frequency
+	#print assignment
 #	return assignment
 
 def unassign(state,assignment):
@@ -22,38 +23,59 @@ def unassign(state,assignment):
 
 #Return the state having maximum no of neighbourhood states and unassigned frequency  
 def selUnassignedVar(states,states_domain,assignment):
+	#print states_domain
 	temp = sorted(states_domain, key = lambda k: len(states_domain[k]))
-	temp = [i for i in temp if len(states_domain[i]) == len(states_domain[temp[0]])]
-	#temp = sorted(states, key = lambda k: len(states[k]))
-	#temp = [i for i in temp if len(states[i]) == len(states[temp[0]])]
-	#states_domain[temp[0]] = [1,2,3,4,5,6]
-	#print len(states_domain[temp[0]]),len(states_domain[temp[1]])
-	return max(set(temp) - set(assignment.keys()), key = lambda k: len(states[k]))
+	print len(assignment.keys())
+	temp = [i for i in temp if len(states_domain[i]) == len(states_domain[temp[len(assignment.keys())]])]
+	#return max(set(temp) - set(assignment.keys()), key = lambda k: len(states[k]))
+	temp = states_domain.keys()
+	#print temp
+	return temp[len(assignment.keys())]
 #selUnassignedVar(states,states_domain)
 
 def arc_consistency(states,state,states_domain,assignment):
-	for key in states:
-		for state in states[key]:
-			states_domain[state].remove(assignment[key])
+	for key in states[state]:
+		#print key
+		#for state in states[key]:
+		states_domain[key].remove(assignment[state])
+	if min(states_domain, key = lambda k: len(states_domain[k])) == 0:
+		return False
+	else:
+		return True
 #	return states_domain			
 
 def isConsistent(states,freq,assignment):
-	for neighbour in states:
+	for neighbour in states.keys():
 		if (assignment.get(neighbour,None) == freq):
 			return False
 	return True
 #Solves CSP
+c = 0
 def csp(states,states_domain,assignment):
+	global c
+	c+=1
+	#print c
 	if len(assignment.keys()) == len(states.keys()):
 		return assignment
 	var = selUnassignedVar(states,states_domain,assignment)
 	for value in states_domain[var]:
 		if (isConsistent(states,value,assignment)):
+			#print var,value,assignment
 			assign(var,value,assignment)
+			print var, value, assignment
 			#new_state_domain 
-			arc_consistency(states,var,states_domain,assignment)
-			if min(states_domain, key = lambda k: len(states_domain[k])) == 0:
-				return False
+			if arc_consistency(states,var,states_domain,assignment):
+				result = csp(states,states_domain,assignment)
+				#print assignment
+				#print result
+				if result != None:
+					return result
+			else:
+				unassign(var,assignment)
+
+sol = csp(states,states_domain,assignment)
+print sol
+			
 
 
 
