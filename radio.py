ded1f37b0256d88966291346d7de6d40c39c67d3
Nishant Shah <shah.nishant92@gmail.com>
2015-11-03 03:21:05 -0500

    Partial CSP Solver

--- radio.py
+++ radio.py
@@ -5,28 +5,58 @@ states = dict()
 for line in f:
 	states[line.split()[0]] = tuple(line.split()[1:])
 
-'''
-for key in states.keys():
-	print key,len(states[key])
-print  max(states, key = lambda k: len(states[k]))
-'''
 #domain value in a CSP	
 domain = ('A','B','C','D')
-#states_freq stores a state as a key and possible assignment of frequencies to that particular state
-states_freq = dict()
+#states_domain stores a state as a key and possible assignment of frequencies to that particular state
+states_domain = dict()
 for key in states.keys():
-	states_freq[key] = list(domain)
-#Return the state having maximum no of neighbourhood states and unassigned frequency  
-def selUnassignedVar(states,states_freq):
-	#print max(states, key = lambda k: len(states[k]))
-	return max(states, key = lambda k: len(states[k]) + len(states_freq[k]))
-
+	states_domain[key] = list(domain)
 assignment = dict()
-def arc_consistency(states,states_freq,assignment):
+def assign(state,frequency,assignment):
+	assignment[state] = frequency
+#	return assignment
 
+def unassign(state,assignment):
+	del assignment[state]
+#	return assignment
+
+#Return the state having maximum no of neighbourhood states and unassigned frequency  
+def selUnassignedVar(states,states_domain,assignment):
+	temp = sorted(states_domain, key = lambda k: len(states_domain[k]))
+	temp = [i for i in temp if len(states_domain[i]) == len(states_domain[temp[0]])]
+	#temp = sorted(states, key = lambda k: len(states[k]))
+	#temp = [i for i in temp if len(states[i]) == len(states[temp[0]])]
+	#states_domain[temp[0]] = [1,2,3,4,5,6]
+	#print len(states_domain[temp[0]]),len(states_domain[temp[1]])
+	return max(set(temp) - set(assignment.keys()), key = lambda k: len(states[k]))
+#selUnassignedVar(states,states_domain)
+
+def arc_consistency(states,state,states_domain,assignment):
+	for key in states:
+		for state in states[key]:
+			states_domain[state].remove(assignment[key])
+#	return states_domain			
+
+def isConsistent(states,freq,assignment):
+	for neighbour in states:
+		if (assignment.get(neighbour,None) == freq):
+			return False
+	return True
 #Solves CSP
-def csp(states,domain):
-	pass
+def csp(states,states_domain,assignment):
+	if len(assignment.keys()) == len(states.keys()):
+		return assignment
+	var = selUnassignedVar(states,states_domain,assignment)
+	for value in states_domain[var]:
+		if (isConsistent(states,value,assignment)):
+			assign(var,value,assignment)
+			#new_state_domain 
+			arc_consistency(states,var,states_domain,assignment)
+			if min(states_domain, key = lambda k: len(states_domain[k])) == 0:
+				return False
+
+
+
 
 
 
